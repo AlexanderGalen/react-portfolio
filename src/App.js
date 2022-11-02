70de3ac9262474f02d0d8548148b46387e17d693
Alexander Carr <alexanderc93@gmail.com>
2022-11-02 14:20:45 -0700

    set up react router and routes for next stages of development

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Resume from "./pages/Resume";
+import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+export default function App() {
+	return (
+		<Router>
+			<div className="App">
+				<Header/>
+
+				<Routes>
+					{/* default to about me if no specific page was requested */}
+					<Route path="/" element={<About/>}/>
+					<Route path="/about" element={<About/>}/>
+					<Route path="/contact" element={<Contact/>}/>
+					<Route path="/portfolio" element={<Portfolio/>}/>
+					<Route path="/resume" element={<Resume/>}/>
+				</Routes>
+
+				<Footer/>
+			</div>
+		</Router>
+	);
 }
-
-export default App;
